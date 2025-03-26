f7e55fc91ad4a5d1b84e2780e85bc91735fef199
MLauraCronemberger <maria.laura.cronemberger@gmail.com>
2025-03-26 09:32:08 -0300

    Controller de Aluno feito

--- src/main/java/com/example/demo/controller/AlunoController.java
+++ src/main/java/com/example/demo/controller/AlunoController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +29,29 @@ public class AlunoController {
 		return alunocadastrado;
 		
 	}
+	
+	@GetMapping(value="/cadastrados")
+	public List<Aluno> findAll(){
+		List<Aluno> alunos = repository.findAll();
+		return alunos;
+	}
+	
+	@GetMapping(value="/{id}")
+	public Aluno findById(@PathVariable Long id) {
+		Aluno aluno = repository.findById(id).get();
+		return aluno;
+	}
+	
+	@DeleteMapping(value="deletar-{id}")
+	public String deleteById(@PathVariable Long id) {
+		repository.deleteById(id);
+		return "Aluno deletado";
+		
+	}
+	
+
+	
+	
 	
 	
 
